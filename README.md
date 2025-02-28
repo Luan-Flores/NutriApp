6a1f0bc58308e01e6889dccc82c6c4fc4cfcbfc6
Luan-Flores <keeganluan919@gmail.com>
2025-02-28 11:04:32 -0400

    Layout e interatividade da página de cadastro feita

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # NutriApp
+#Projeto desktop Nutricionista
+2ª Avaliação: Sistema Desktop para Nutricionista
+
+Instruções
+Crie um sistema desktop utilizando o QtDesigner, com funcionalidades que atendam as necessidades básicas de um nutricionista no dia a dia de seu consultório.
+
+Cadastro de Pacientes: O sistema deve permitir o cadastro de pacientes, incluindo informações como:Nome, Idade, Sexo, Peso, Altura e Objetivo (perda de peso, ganho de massa muscular, manutenção, etc.).
+O nutricionista poderá editar, visualizar e excluir as informações dos pacientes.
+
+
+Controle de Dietas: Permitir o cadastro e o gerenciamento de dietas personalizadas para cada paciente, com a possibilidade de:
+- Criar e editar planos alimentares (exemplo: plano de 7 dias, 30 dias).
+- Inserir alimentos e quantidades para cada refeição.
+- Associar alimentos às preferências alimentares do paciente (exemplo: intolerâncias alimentares, restrições dietéticas).
+
+Acompanhamento de Evolução:
+- O nutricionista poderá registrar o peso e outras medições (circunferências, percentual de gordura) ao longo do tempo para acompanhar a evolução do paciente.
+
+
+Agenda de Consultas
+- Um calendário simples para agendamento de consultas. O nutricionista deve ser capaz de:
+- Agendar novas consultas com data e horário.
+- Editar ou excluir consultas.
+- Visualizar todas as consultas agendadas.
---
+++ code/cadastro/main.py
@@ -0,0 +1,113 @@
+import sys, json
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QRadioButton, QButtonGroup
+from cadastro import Ui_MainWindow
+
+class Main(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.enviar.clicked.connect(self.cadastrar)
+        
+        # Criando os grupos para os radio buttons
+        self.grupoSexo = QButtonGroup()
+        self.grupoObjetivo = QButtonGroup()
+        self.grupoPlano = QButtonGroup()
+
+        # Adicionando ao grupo (somente um poderá ser selecionado)
+        self.grupoObjetivo.addButton(self.ui.radioGanharPeso)
+        self.grupoObjetivo.addButton(self.ui.radioPerderPeso)
+        self.grupoObjetivo.addButton(self.ui.radioOutro)
+
+        # Adicionando ao grupo (somente um poderá ser selecionado)
+        self.grupoPlano.addButton(self.ui.radioBasico)
+        self.grupoPlano.addButton(self.ui.radioIntermediario)
+        self.grupoPlano.addButton(self.ui.radioAvancado)
+        
+        # Adicionando ao grupo (somente um poderá ser selecionado)
+        self.grupoSexo.addButton(self.ui.radioM)
+        self.grupoSexo.addButton(self.ui.radioF)
+
+
+        
+    def clear(self):
+        self.ui.lineNome.clear()
+        self.ui.lineIdade.clear()
+        self.ui.lineAltura.clear()
+        self.ui.linePeso.clear()
+        self.ui.lineObjetivo.clear()
+        self.ui.radioF.setChecked(False)
+        self.ui.radioM.setChecked(False)
+        self.ui.radioPerderPeso.setChecked(False)
+        self.ui.radioGanharPeso.setChecked(False)
+        self.ui.radioOutro.setChecked(False)
+        
+    def novoJogo(self):
+        self.clear()
+        QtWidgets.QMessageBox.information(self, "Novo jogo", "Novo jogo iniciado!")
+    
+    def cadastrar(self):
+        
+        data = {
+            "nome" : self.ui.lineNome.text(),
+            "idade" : self.ui.lineIdade.text(),
+            "peso": self.ui.linePeso.text(),
+            "altura" : self.ui.lineAltura.text(),
+            "sexo" : "Feminino" if self.ui.radioF.isChecked() else "Masculino" if self.ui.radioM.isChecked() else "Não informado",
+            "objetivo" : "Perder peso" if self.ui.radioPerderPeso.isChecked() else "Ganhar peso" if self.ui.radioGanharPeso.isChecked() else f"{self.ui.lineObjetivo.text()}" if self.ui.radioOutro.isChecked() else "Nada consta" 
+        }
+
+        file_path, _ = QtWidgets.QFileDialog.getSaveFileName(self, "Cadastrar", "", "Arquivos JSON (*.json)")
+
+        if file_path:
+            try:
+                #Verifica se o arquivo termina com '.json'
+                if not file_path.endswith('.json'):
+                    file_path += '.json'
+                
+                #Salva o arquivo JSON no local escolhido
+                with open(file_path, "w", encoding="utf-8") as file:
+                    json.dump(data, file, ensure_ascii=False, indent=4)
+
+                QtWidgets.QMessageBox.information(self, "Sucesso", "Cadastro feito com sucesso! ")
+            except Exception as e:
+                QtWidgets.QMessageBox.critical(self, "Erro", f"Erro ao realizar o cadastro: {e}")
+        else:
+            QtWidgets.QMessageBox.warning(self, "Erro", "Nenhum arquivo selecionado ")
+
+    def carregarJogo(self):
+        file_path, _ = QtWidgets.QFileDialog.getSaveFileName(self, "Carregar jogo", "", "Arquivos JSON (*.json)")
+        
+        if file_path:
+            try:
+                with open(file_path, "r", encoding="utf-8") as file:
+                    
+                    data = json.load(file)
+                    
+                    self.ui.lineNome.setText(data.get("nome", ""))
+                    self.ui.dateEdit.setDate(QtCore.QDate.fromString(data.get("data_nascimento", "2000-01-01"), "yyyy-MM-dd"))
+                    self.ui.lineIdade.setText(data.get("idade", ""))
+                    self.ui.lineAltura.setText(data.get("altura", ""))
+
+                    if data.get("sexo") == "Feminino":
+                        self.ui.checkF.setChecked(True)
+                    elif data.get("sexo") == "Masculino":
+                        self.ui.checkM.setChecked(True)
+                    else:
+                        self.ui.checkF.setChecked(False)
+                        self.ui.checkM.setChecked(False)
+
+                QtWidgets.QMessageBox.information(self, "Sucesso", "Jogo carregado com sucesso")
+
+            except Exception as e:
+                QtWidgets.QMessageBox.critical(self, "Erro", f"Erro ao carregar o jogo : {e}")
+        else:
+            QtWidgets.QMessageBox.warning(self, 'Erro', 'Nenhum arquivo selecionado')
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = Main()
+    window.show()
+    sys.exit(app.exec_())
